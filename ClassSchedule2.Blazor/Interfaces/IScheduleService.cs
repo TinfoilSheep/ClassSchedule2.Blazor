09c124b21b2eae07bf33993827531f3d372c8c65
Tobias K. Larsen <tobiaslarsen93@gmail.com>
2026-08-27 08:10:50 +0200

    ScheduleService, interface and dto

---
+++ ClassSchedule2.Blazor/Interfaces/IScheduleService.cs
@@ -0,0 +1,10 @@
+﻿using ClassSchedule2.Blazor.Models.Enums;
+using static ClassSchedule2.Blazor.Models.DTOs.ScheduleLessonLibrary;
+
+namespace ClassSchedule2.Blazor.Interfaces
+{
+    public interface IScheduleService
+    {
+        public Task<List<ScheduleLessonDTO>> GetScheduleAsync(GetScheduleLessonDTO dto);
+    }
+}
